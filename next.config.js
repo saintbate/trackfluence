794197073ae8b12256f74ea0497678716104cd02
Nick Bateman <batemannick66@gmail.com>
2025-11-14 07:57:01 -0500

    Add root redirect to /signin in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    eslint: {
-      ignoreDuringBuilds: true,
-    },
-    // keep/restore any options you actually need:
-    // images: { domains: ['lh3.googleusercontent.com'] },
-    // experimental: { serverActions: true },
-  };
-  
-  module.exports = nextConfig;
+  reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  // keep/restore any options you actually need:
+  // images: { domains: ['lh3.googleusercontent.com'] },
+  // experimental: { serverActions: true },
+};
+
+module.exports = nextConfig;
